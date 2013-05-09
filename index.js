878cdb8211ea933d759d82b50d2c3eae2ded0d86
steve kane <skane@4-south.com>
2013-05-09 04:06:55 -0500

    stripped user auth stuff from index.js

--- index.js
+++ index.js
@@ -1,9 +1,5 @@
 var express = require('express')
-  , passport = require('passport')
-  , pass = require('./app/config/PassPort.js')
-  , app = express()
-  , userRoutes = require('./app/routes/UserRoutes.js')
-  , db = require('./app/DB/DB.js');
+  , app = express();
 
 //configuration for default env, could be changed for diff deployments
 app.configure(function() {
@@ -14,8 +10,6 @@ app.configure(function() {
   app.use(express.session({
     secret: '42',
   }));
-  app.use(passport.initialize());
-  app.use(passport.session());
   app.use(app.router);
   app.use(express.static(__dirname + "/public"));
   app.use(express.static(__dirname));
@@ -25,11 +19,6 @@ app.configure(function() {
 app.get('/', function(req, res) {
   res.sendfile(__dirname + "/index.html");
 });
-
-app.post('/user/login', passport.authenticate('local'), userRoutes.postlogin);
-app.post('/user/create', userRoutes.postcreate);
-app.get('/user/logout', userRoutes.getlogout);
-app.get('/user/sample', pass.verifyAuth, userRoutes.getsample);
 
 var port = process.env.PORT || 1234;
 
